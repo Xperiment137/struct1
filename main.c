93eac1018b6745170bf6e45818a671adcef2a6c6
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-06-23 14:17:17 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ typedef struct{
+char nombre[20];
+char autor[20];
+char genero[20];
+int votosp;
+int  votosn;
+int aceptacion;
+}CANCION;
+
+
+
+
+
+void sacarcanciones (CANCION list);
+
+
+
+int main()
+{
+    FILE *f;
+    int i=0;
+   CANCION list;
+	int elegir=0;
+	 do{
+scanf("%d",&elegir);
+switch(elegir)
+{
+    case 1:
+        if(!(f=fopen("registro.txt","r")))
+{
+    printf("Error");
+     return(1);
+}
+
+
+scanf("%s",list.nombre);
+scanf("%s",list.autor);
+scanf("%s",list.genero);
+scanf("%d",&list.votosp);
+scanf("%d",&list.votosn);
+
+        fclose(f);
+    break;
+    case 2:sacarcanciones(list);
+    break;
+    case 3:
+
+    break;
+    default:printf("Abutarda......SALIENDOOOOOOOOOOOOOO\n");
+}
+}
+while(elegir!=0);
+system("pause");
+    return 0;
+
+}
+
+
+void sacarcanciones(CANCION list)
+{
+    int i=0;
+
+    printf("%s\n%s\n%s\n%u\n%d\n",list.nombre,list.autor,list.genero,list.votosp,list.votosn);
+
+
+}
